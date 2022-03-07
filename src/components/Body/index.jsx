5722b6e96574d8afd7ad5f1bcdd3876b62855ad3
Marco Luciano <maurelio.sluciano@gmail.com>
2022-03-07 18:43:01 -0300

    update body component

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -5,6 +5,7 @@ export const Body = styled.div `
     background-color: #0d1a2d;
     width:100%;
     height:100vh;
+    min-height: 600px;
     display: flex;
     justify-content:center;
     align-items:center;
